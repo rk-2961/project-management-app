17bd0b912025f9a1d966e8803a962b5fcb118264
Rahul Khandelwal <rahul.khandelwal@atmecs.com>
2024-05-17 13:24:38 +0530

    Created two more components to handle project addition

--- project9/01-starting-project/src/App.jsx
+++ project9/01-starting-project/src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectState,setProjectsState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+  });
+  function handleStartAddProject(){
+    setProjectsState((prevState)=>{
+      return {
+        ...prevState,
+        selectedProjectId:null,
+
+      };
+    })
+  }
+  let content;
+  if(projectState.selectedProjectId=== null){
+    content=<NewProject/>
+  }
+  else if(projectState.selectedProjectId=== undefined){
+    content=<NoProjectSelected  onStartAddProject={handleStartAddProject}/>
+  }
   return (
-    <main className="h-screen my-8">
-    <ProjectSidebar/>
-     
+    <main className="h-screen my-8 flex gap-8">
+    <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+    {content}
     </main>
   );
 }

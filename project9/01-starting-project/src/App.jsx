edaf399de35bac5e568b0a57a4241ff1222b5c0e
Rahul Khandelwal <rahul.khandelwal@atmecs.com>
2024-05-17 18:01:43 +0530

    Finally finished the project management app

--- project9/01-starting-project/src/App.jsx
+++ project9/01-starting-project/src/App.jsx
@@ -2,12 +2,39 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectState,setProjectsState]=useState({
     selectedProjectId:undefined,
-    projects:[]
+    projects:[],
+    tasks:[]
   });
+  function handleAddTask(text){
+    setProjectsState((prevState)=>{
+      const taskId=Math.random();
+      const newTask={
+        text:text,
+        
+        projectId:prevState.selectedProjectId,
+        id:taskId,
+      };
+      return{
+        ...prevState,
+        tasks:[...prevState.tasks,newTask]
+      };}
+    )
+  }
+  function handleDeleteTask(id){
+    setProjectsState((prevState)=>{
+      return{
+        ...prevState,
+        tasks:prevState.tasks.filter((task)=>task.id !== id),
+
+      };
+    });
+    
+  }
   function handleStartAddProject(){
     setProjectsState((prevState)=>{
       return {
@@ -40,17 +67,38 @@ function App() {
       };
     })
   }
+  function handleSelectProject(id){
+    setProjectsState((prevState)=>{
+      return{
+        ...prevState,
+        selectedProjectId:id,
+      };
+    });
+  }
+  function handleDeleteProject(){
+    setProjectsState((prevState)=>{
+      return{
+        ...prevState,
+        selectedProjectId:undefined,
+        projects:prevState.projects.filter((project)=>project.id !== prevState.selectedProjectId),
 
-  let content;
+      };
+    });
+  }
+  const selectedProject=projectState.projects.find(project=>project.id===projectState.selectedProjectId);
+  let content=<SelectedProject project={selectedProject} onDelete={handleDeleteProject} onDeleteTask={handleDeleteTask} onAddtask={handleAddTask} tasks={projectState.tasks}/>;
   if(projectState.selectedProjectId=== null){
     content=<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   }
   else if(projectState.selectedProjectId=== undefined){
     content=<NoProjectSelected  onStartAddProject={handleStartAddProject}/>
   }
+  else{
+
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-    <ProjectSidebar projects={projectState.projects} onStartAddProject={handleStartAddProject}/>
+    <ProjectSidebar selectedProjectId={projectState.selectedProjectId}  onSelectProject={handleSelectProject} projects={projectState.projects} onStartAddProject={handleStartAddProject}/>
     {content}
     </main>
   );
---
+++ project9/01-starting-project/src/components/NewTask.jsx
@@ -0,0 +1,31 @@
+import { useRef, useState } from "react"
+import Modal from "./Modal.jsx";
+export default function NewTask({onAdd}){
+    const modal=useRef();
+   const [enteredTask,setEnteredTask] =useState('');
+   function handleChange(event){
+    setEnteredTask(event.target.value);
+   }
+   function handleClick(){
+    if(enteredTask.trim()===''){
+        modal.current.open();
+        return;
+    }
+    onAdd(enteredTask);
+    setEnteredTask('');
+
+   }
+    return(<>
+    <Modal buttonCaption={"Okay!"}  ref={modal}>
+        <h2 className='text-xl font-bold text-stone-700 my-4' >Invalid Input</h2>
+        <p  className='text-stone-600 mb-4 '>Oops.. looks like you forgot to enter a value</p>
+        <p className='text-stone-600 mb-4 '> Please make sure  you provide a valid value for every input field.</p>
+    </Modal>
+    <div className="flex items-center gap-4">
+            <input onChange={handleChange} type="text" className="w-64 py-1 px-2 rounded-sm bg-stone-200" value={enteredTask}/>
+            <button className="text-stone-700 hover:text-stone-950 " onClick={handleClick}>Add Task</button>
+        </div>
+    </>
+        
+    )
+}
